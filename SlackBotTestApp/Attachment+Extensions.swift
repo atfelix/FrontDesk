ee327cabc4bc296f21d066331f80ee6231042b55
Adam Felix <adam.tyler.felix@gmail.com>
2017-09-12 12:46:25 -0400

    Change signature for Attachments helper functions

--- SlackBotTestApp/Attachment+Extensions.swift
+++ SlackBotTestApp/Attachment+Extensions.swift
@@ -10,41 +10,58 @@ import SKCore
 
 extension Attachment {
     static func regularDeliveryAttachment(for deliveryCell: DeliveryTableViewCell) -> [Attachment]? {
-        guard let user = deliveryCell.user else { return nil }
+        guard
+            let user = deliveryCell.user,
+            let userName = user.name else {
+                return nil
+        }
 
-        return [Attachment(fallback: self.deliveryFallback(for: user),
+        return [Attachment(fallback: self.deliveryFallback(userName: userName),
                            title: nil,
                            colorHex: self.colorHex(),
-                           pretext: self.deliveryPretext(for: user),
+                           pretext: self.deliveryPretext(userName: userName),
                            fields: self.deliveryFields(for: deliveryCell))]
     }
 
     static func awayDeliveryAttachement(for deliveryCell: DeliveryTableViewCell, channel: String) -> [Attachment]? {
-        guard let user = deliveryCell.user else { return nil }
+        guard
+            let user = deliveryCell.user,
+            let userName = user.name else {
+                return nil
+        }
 
-        return [Attachment(fallback: self.awayDeliveryFallback(for: user, channel: channel),
+        return [Attachment(fallback: self.awayDeliveryFallback(userName: userName, channel: channel),
                            title: nil,
                            colorHex: self.colorHex(),
-                           pretext: self.awayDeliveryPretext(for: user, channel: channel),
+                           pretext: self.awayDeliveryPretext(userName: userName, channel: channel),
                            fields: self.deliveryFields(for: deliveryCell))]
     }
 
     static func dndDeliveryAttachement(for deliveryCell: DeliveryTableViewCell, channel: String) -> [Attachment]? {
-        guard let user = deliveryCell.user else { return nil }
+        guard
+            let user = deliveryCell.user,
+            let userName = user.name else {
+                return nil
+        }
 
-        return [Attachment(fallback: self.dndDeliveryFallback(for: user, channel: channel),
+        return [Attachment(fallback: self.dndDeliveryFallback(userName: userName, channel: channel),
                            title: nil,
                            colorHex: self.colorHex(),
-                           pretext: self.dndDeliveryPretext(for: user, channel: channel),
+                           pretext: self.dndDeliveryPretext(userName: userName, channel: channel),
                            fields: self.deliveryFields(for: deliveryCell))]
     }
 
     static func meetingAttachment(for meetingCell: MeetingTableViewCell, name: String, from company: String, with email: String) -> [Attachment]? {
-        guard let user = meetingCell.user else { return nil }
-        return [Attachment(fallback: self.meetingFallback(for: user),
+        guard
+            let user = meetingCell.user,
+            let userName = user.name else {
+                return nil
+        }
+
+        return [Attachment(fallback: self.meetingFallback(userName: userName),
                            title: nil,
                            colorHex: self.colorHex(),
-                           pretext: self.meetingPretext(for: user),
+                           pretext: self.meetingPretext(userName: userName),
                            fields: self.meetingFields(for: meetingCell,
                                                       name: name,
                                                       from: company,
@@ -52,11 +69,16 @@ extension Attachment {
     }
 
     static func awayMeetingAttachment(for meetingCell: MeetingTableViewCell, channel: String, name: String, from company: String, with email: String) -> [Attachment]? {
-        guard let user = meetingCell.user else { return nil }
-        return [Attachment(fallback: self.awayMeetingFallback(for: user, channel: channel),
+        guard
+            let user = meetingCell.user,
+            let userName = user.name else {
+                return nil
+        }
+
+        return [Attachment(fallback: self.awayMeetingFallback(userName: userName, channel: channel),
                            title: nil,
                            colorHex: self.colorHex(),
-                           pretext: self.awayMeetingPretext(for: user, channel: channel),
+                           pretext: self.awayMeetingPretext(userName: userName, channel: channel),
                            fields: self.channelMeetingFields(for: meetingCell,
                                                              name: name,
                                                              from: company,
@@ -64,69 +86,68 @@ extension Attachment {
     }
 
     static func dndMeetingAttachment(for meetingCell: MeetingTableViewCell, channel: String, name: String, from company: String, with email: String) -> [Attachment]? {
-        guard let user = meetingCell.user else { return nil }
-        return [Attachment(fallback: self.dndMeetingFallback(for: user, channel: channel),
+        guard
+            let user = meetingCell.user,
+            let userName = user.name else {
+                return nil
+        }
+
+        return [Attachment(fallback: self.dndMeetingFallback(userName: userName, channel: channel),
                            title: nil,
                            colorHex: self.colorHex(),
-                           pretext: self.dndMeetingPretext(for: user, channel: channel),
+                           pretext: self.dndMeetingPretext(userName: userName, channel: channel),
                            fields: self.channelMeetingFields(for: meetingCell,
                                                              name: name,
                                                              from: company,
                                                              with: email))]
     }
 
-    private static func deliveryFallback(for user: User) -> String {
-        guard let userName = user.name else { return "banana" }
+    private static func deliveryFallback(userName: String) -> String {
         return "Hey @\(userName), you have a parcel for delivery."
     }
 
-    private static func meetingFallback(for user: User) -> String {
-        guard let userName = user.name else { return "banana" }
+    private static func meetingFallback(userName: String) -> String {
         return "Hey @\(userName), someone is here to see you."
     }
 
-    private static func awayDeliveryFallback(for user: User, channel: String) -> String {
-        guard let userName = user.name else { return "banana" }
+    private static func awayDeliveryFallback(userName: String, channel: String) -> String {
         return "Hey everybody, @\(userName) has a parcel delivery but is away."
     }
 
-    private static func awayMeetingFallback(for user: User, channel: String) -> String {
-        guard let userName = user.name else { return "banana" }
+    private static func awayMeetingFallback(userName: String, channel: String) -> String {
         return "Hey everybody, someone is here to see @\(userName) but is away."
     }
 
-    private static func dndDeliveryFallback(for user: User, channel: String) -> String {
-        guard let userName = user.name else { return "banana" }
+    private static func dndDeliveryFallback(userName: String, channel: String) -> String {
         return "Hey everybody, @\(userName) has a parcel delivery but doesn't want to be disturbed."
     }
 
-    private static func dndMeetingFallback(for user: User, channel: String) -> String {
-        guard let userName = user.name else { return "banana" }
+    private static func dndMeetingFallback(userName: String, channel: String) -> String {
         return "Hey everybody, someone is here to see @\(userName) who doesn't want to be disturbed."
     }
 
-    private static func deliveryPretext(for user: User) -> String {
-        return self.deliveryFallback(for: user)
+    private static func deliveryPretext(userName: String) -> String {
+        return self.deliveryFallback(userName: userName)
     }
 
-    private static func meetingPretext(for user: User) -> String {
-        return self.meetingFallback(for: user)
+    private static func meetingPretext(userName: String) -> String {
+        return self.meetingFallback(userName: userName)
     }
 
-    private static func awayMeetingPretext(for user: User, channel: String) -> String {
-        return self.awayMeetingFallback(for: user, channel: channel)
+    private static func awayMeetingPretext(userName: String, channel: String) -> String {
+        return self.awayMeetingFallback(userName: userName, channel: channel)
     }
 
-    private static func awayDeliveryPretext(for user: User, channel: String) -> String {
-        return self.awayDeliveryFallback(for: user, channel: channel)
+    private static func awayDeliveryPretext(userName: String, channel: String) -> String {
+        return self.awayDeliveryFallback(userName: userName, channel: channel)
     }
 
-    private static func dndDeliveryPretext(for user: User, channel: String) -> String {
-        return self.dndDeliveryFallback(for: user, channel: channel)
+    private static func dndDeliveryPretext(userName: String, channel: String) -> String {
+        return self.dndDeliveryFallback(userName: userName, channel: channel)
     }
 
-    private static func dndMeetingPretext(for user: User, channel: String) -> String {
-        return self.dndMeetingFallback(for: user, channel: channel)
+    private static func dndMeetingPretext(userName: String, channel: String) -> String {
+        return self.dndMeetingFallback(userName: userName, channel: channel)
     }
 
     private static func colorHex() -> String {
